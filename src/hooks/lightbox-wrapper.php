7cf241867e1c017982fba6bae93c23660d9bbe77
Artyom Vasich <vasich.dev@gmail.com>
2023-10-03 21:23:57 +0400

    supress html5 load warning

--- src/hooks/lightbox-wrapper.php
+++ src/hooks/lightbox-wrapper.php
@@ -1,34 +1,39 @@
 <?php
 
+function get_image_url($innerHTML)
+{
+    $dom = new DOMDocument();
+    @$dom->loadHTML($innerHTML);
+
+    return $dom
+        ->getElementsByTagName("img")
+        ->item(0)
+        ?->attributes
+        ?->getNamedItem("src")
+        ?->nodeValue;
+}
+
+
 add_filter('render_block', 'wrap_in_lightbox', 10, 2);
 function wrap_in_lightbox($block_content, $block)
 {
-    if ($block['blockName'] == 'core/image' && str_contains($block['attrs']['className'], 'bim-lightbox')) {
-        $get_image_url = function ($id, $innerHTML) {
-            if (!is_null($id)) {
-                return wp_get_attachment_image_url($id, 'full');
-            } else {
-                $dom = new DOMDocument();
-                $dom->loadHTML($innerHTML);
-
-                $image = $dom->getElementsByTagName("img")->item(0);
-                if (!is_null($image)) {
-                    return $image->attributes->getNamedItem('src')->nodeValue;
-                } else {
-                    return null;
-                }
-
-            }
-        };
-
-        $url = $get_image_url($block['attrs']['id'], $block['innerHTML']);
-
-        if (is_null($url)) {
-            return $block_content;
-        } else {
-            return do_shortcode("[su_lightbox type='image' class='bim-lightbox-wrapper' src='$url']$block_content [/su_lightbox]");
-        }
-    } else {
+    if ($block['blockName'] != 'core/image') {
         return $block_content;
+    }
+
+    $is_lightbox = isset($block['attrs']['className']) && str_contains($block['attrs']['className'], 'bim-lightbox');
+
+    if (!$is_lightbox) {
+        return $block_content;
+    }
+
+    $url = isset($block['attrs']['id'])
+        ? wp_get_attachment_image_url($block['attrs']['id'], 'full')
+        : get_image_url($block['innerHTML']);
+
+    if (is_null($url)) {
+        return $block_content;
+    } else {
+        return do_shortcode("[su_lightbox type='image' class='bim-lightbox-wrapper' src='$url'] $block_content [/su_lightbox]");
     }
 }
